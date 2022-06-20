b83ea1e8419c8ff46360377545f93b537b6ba2b6
handan unal <handan.unal@yahoo.com>
2022-06-20 23:21:48 +0300

    froala edtor

---
+++ Blog/Controllers/BlogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult CreatorPage()
+        {
+            return View();
+        }
+
+
+
+    }
+}
